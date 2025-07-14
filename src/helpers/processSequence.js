7c4c468b3d9245fbee3540c5707d097e43d7cbde
Komanyak Alexandr <komanyak.alex@yandex.ru>
2025-07-14 13:26:52 +0300

    2 task

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -14,38 +14,74 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
+import Api from "../tools/api";
+import {
+  tap,
+  allPass,
+  test,
+  length,
+  pipe,
+  ifElse,
+  gt,
+  lt,
+  __,
+  andThen,
+  otherwise,
+  prop,
+  partial,
+  mathMod,
+} from "ramda";
 
- const api = new Api();
+const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+const processSequence = ({ value, writeLog, handleSuccess, handleError }) => {
+  const log = tap(writeLog);
+  const success = tap(handleSuccess);
+  const error = tap(handleError);
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+  const validate = allPass([
+    test(/^[0-9.]+$/), // Только цифры и точки
+    pipe(length, gt(__, 2)), // Длина > 2
+    pipe(length, lt(__, 10)), // Длина < 10
+    pipe(parseFloat, gt(__, 0)), // Положительное число
+  ]);
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+  const roundNumber = pipe(parseFloat, Math.round, log);
+  const getBinary = (number) =>
+    api.get("https://api.tech/numbers/base", {
+      from: 10,
+      to: 2,
+      number,
+    });
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+  const getAnimal = (id) => api.get(`https://animals.tech/${id}`, {});
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+  const processValidatedValue = pipe(
+    roundNumber,
+    getBinary,
+    andThen(
+      pipe(
+        prop("result"),
+        log,
+        pipe(length, log),
+        (length) => length * length,
+        log,
+        mathMod(__, 3),
+        log,
+        getAnimal,
+        andThen(pipe(prop("result"), success))
+      )
+    ),
+    otherwise(partial(error, ["API Error"]))
+  );
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+  log(value);
+
+  ifElse(
+    validate,
+    processValidatedValue,
+    partial(error, ["ValidationError"])
+  )(value);
+};
 
 export default processSequence;
